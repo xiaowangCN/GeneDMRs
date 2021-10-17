8586d223ef2a1f4fd5bd7f0698cce55a34800288
Xiao Wang <47446460+xiaowangCN@users.noreply.github.com>
2021-10-17 17:37:13 +0200

    Add files via upload

--- R/Enrich_plot.R
+++ R/Enrich_plot.R
@@ -1,11 +1,10 @@
 #' Enrich plot for GO terms and pathways.
-#' 
+#'
 #' @import clusterProfiler
 #' @import org.Mm.eg.db
-#' @import KEGG.db
-#' 
-#' @description This function outputs the groups, GO terms and pathways plot for the enrichment based on R package clusterProfiler, org.Mm.eg.db (Mouse for example) and KEGG.db.
-#' 
+#'
+#' @description This function outputs the groups, GO terms and pathways plot for the enrichment based on R package clusterProfiler and org.Mm.eg.db (Mouse for example).
+#'
 #' @param regiongenealls_significant refers to the differentially methylated genes.
 #' @param adjustpvaluecut refers to the threshold of the adjusted P values for the enrichment, with default 0.1.
 #' @param enrichterm refers to the term need to be analyzed, which can be "GOgroup", "GO", "pathway", with default "pathway".
@@ -14,12 +13,12 @@
 #' @param keggorganism refers to the species name for KEGG enrichment, with default "mmu" of mouse.
 #' @param listnum refers to the list of display number, with default 20.
 #' @param title refers to the title of figure, with default "Enrichments for significant gene".
-#' @param expressionfile_significant refers to an additional file for differentially expressed genes which includes gene name and Log fold change (LogFC). 
+#' @param expressionfile_significant refers to an additional file for differentially expressed genes which includes gene name and Log fold change (LogFC).
 #' This is a optional file for GO terms and pathways, with default NULL.
-#' 
+#'
 #' @param expressionfile_genetype refers to the gene type of expressionfile_significant file, which can be "REFSEQ", "ENTREZID", "SYMBOL",
 #' or other gene types that can be used in clusterProfiler, with default NULL.
-#' 
+#'
 #' @return Outputs a dot-plot figure of enrichment.
 #'
 #' @examples
@@ -28,71 +27,71 @@
 #' Enrich_plot(regiongenealls_significant, enrichterm = "GO", Dbannotation = "org.Hs.eg.db", title = "Go term for significant gene") # for human data #
 #' Enrich_plot(regiongenealls_significant, adjustpvaluecut = 0.2, enrichterm = "pathway", Dbannotation = "org.Mm.eg.db", keggorganism = "mmu", title = "Pathway for significant gene")
 #' Enrich_plot(regiongenealls_significant, enrichterm = "pathway", keggorganism = "hsa", Dbannotation = "org.Hs.eg.db", category = FALSE, title = "Pathway for significant gene") # for human data #
-#' 
+#'
 #' expressionfile_significant <- read.table(paste(system.file(package = "GeneDMRs"), "/methdata/DEgenes.txt", sep=""), header = T) # read DEgene file #
-#' 
-#' Enrich_plot(regiongenealls_significant, adjustpvaluecut = 0.2, enrichterm = "GO", Dbannotation = "org.Mm.eg.db", title = "Go term for significant gene in two categories", 
+#'
+#' Enrich_plot(regiongenealls_significant, adjustpvaluecut = 0.2, enrichterm = "GO", Dbannotation = "org.Mm.eg.db", title = "Go term for significant gene in two categories",
 #' expressionfile_significant = expressionfile_significant, expressionfile_genetype = "SYMBOL")
-#' Enrich_plot(regiongenealls_significant, enrichterm = "pathway", Dbannotation = "org.Mm.eg.db", keggorganism = "mmu", title = "Pathway for significant gene in two categories", 
+#' Enrich_plot(regiongenealls_significant, enrichterm = "pathway", Dbannotation = "org.Mm.eg.db", keggorganism = "mmu", title = "Pathway for significant gene in two categories",
 #' expressionfile_significant = expressionfile_significant, expressionfile_genetype = "SYMBOL")
-#' 
+#'
 #' @export
 
 
-Enrich_plot <- function(regiongenealls_significant, adjustpvaluecut = 0.1, enrichterm = "pathway", category = TRUE, Dbannotation = "org.Mm.eg.db", 
-                        keggorganism = "mmu", listnum = 20, title = "Enrichment for significant gene", 
+Enrich_plot <- function(regiongenealls_significant, adjustpvaluecut = 0.1, enrichterm = "pathway", category = TRUE, Dbannotation = "org.Mm.eg.db",
+                        keggorganism = "mmu", listnum = 20, title = "Enrichment for significant gene",
                         expressionfile_significant = NULL, expressionfile_genetype = NULL){
-  
+
   # get the gene name #
   genefile <- as.vector(unlist(regiongenealls_significant[1]))
   eg = bitr(genefile, fromType = "REFSEQ", toType= c("ENTREZID", "SYMBOL"), OrgDb = Dbannotation)
-  
+
   # uniqe ENTREZID of eg #
   eg <- distinct(eg, ENTREZID, .keep_all = TRUE)
-  
+
   # check enrichterm #
   if(length(grep("GOgroup", enrichterm)) > 0){
-    
+
     # group GO terms for Biological process (BP) #
     ggo <- groupGO(gene = eg$ENTREZID, OrgDb = Dbannotation, ont = "BP", level = 3, readable = TRUE)
-    barplot(ggo, drop = TRUE, showCategory = listnum, title = title) 
-    
+    barplot(ggo, drop = TRUE, showCategory = listnum, title = title)
+
     # if not "GOgroup" then be "GO" or "pathway" #
   }else{
-    
+
     # if without gene expression data #
     if(is.null(expressionfile_significant) == TRUE){
-      
+
       # create new eg for use #
       DMgene_merge <- data.frame(REFSEQ = regiongenealls_significant$id, Methdiff = regiongenealls_significant$Methdiff1)
       eg_merge <- merge(x = eg, y = DMgene_merge, by = "REFSEQ")
       neweg <- data.frame(Entrez = eg_merge$ENTREZID, Methdiff = eg_merge$Methdiff)
       neweg$group <- "Hyper-methylated"
       neweg$group[neweg$Methdiff < 0] <- "Hypo-methylated"
-      
+
       # check enrichterm #
       if(length(grep("GO", enrichterm)) > 0){
         if(category == TRUE){
-          
+
           # GO terms between two categories #
-          formula_go <- compareCluster(Entrez~group, data = neweg, fun = "enrichGO", ont = "all", 
+          formula_go <- compareCluster(Entrez~group, data = neweg, fun = "enrichGO", ont = "all",
                                        pvalueCutoff = adjustpvaluecut, OrgDb = Dbannotation)
-          clusterProfiler::dotplot(formula_go, x = ~group, color = "p.adjust", showCategory = listnum, split = NULL, font.size = 14, 
+          clusterProfiler::dotplot(formula_go, x = "group", color = "p.adjust", showCategory = listnum, split = NULL, font.size = 14,
                                    title = title)
         }else{
-          
+
           # GO terms in one category #
           ego <- enrichGO(gene = neweg$Entrez, OrgDb = Dbannotation, ont= "all", pvalueCutoff = adjustpvaluecut)
           clusterProfiler::dotplot(ego, color = "p.adjust", showCategory = listnum, split = NULL, font.size = 12, title = title)
         }
-        
+
       }else if(length(grep("pathway", enrichterm)) > 0){
         if(category == TRUE){
-          
+
           # pathways between two categories #
-          formula_path <- compareCluster(Entrez~group, data = neweg, fun = "enrichKEGG", 
+          formula_path <- compareCluster(Entrez~group, data = neweg, fun = "enrichKEGG",
                                          organism = keggorganism, pvalueCutoff = adjustpvaluecut)
-          clusterProfiler::dotplot(formula_path, x=~group, color = "p.adjust", showCategory = listnum, split = NULL, font.size = 14,
+          clusterProfiler::dotplot(formula_path, x = "group", color = "p.adjust", showCategory = listnum, split = NULL, font.size = 14,
                                    title = title)
         }else{
           # pathways in one category #
@@ -100,42 +99,42 @@ Enrich_plot <- function(regiongenealls_significant, adjustpvaluecut = 0.1, enric
           clusterProfiler::dotplot(kk, color = "p.adjust", showCategory = listnum, split = NULL, font.size = 12, title = title)
         }
       }
-      
+
     }else{
-      
+
       # new dataset for DE (expressed) and DM (methylated) genes with +/- #
       names(expressionfile_significant)[1] <- expressionfile_genetype
       DEgene_merge <- merge(x = eg, y = expressionfile_significant, by = expressionfile_genetype)
       DMgene_merge <- data.frame(REFSEQ = regiongenealls_significant$id, Methdiff = regiongenealls_significant$Methdiff1)
       DMDEgene_merge <- merge(x = DEgene_merge, y = DMgene_merge, by = "REFSEQ")
-      
+
       neweg <- data.frame(Entrez = DMDEgene_merge$ENTREZID, logFC = DMDEgene_merge$LogFC, Methdiff = DMDEgene_merge$Methdiff)
       if(category == TRUE){
         neweg$group <- "Hyper-methylated"
         neweg$group[neweg$Methdiff < 0] <- "Hypo-methylated"
         neweg$othergroup <- "Up-regulated"
         neweg$othergroup[neweg$logFC < 0] <- "Down-regulated"
-        
+
       }else{
         neweg$group <- "Differentially methylated"
         neweg$othergroup <- "Differentially expressed"
       }
-      
+
       # check enrichterm #
       if(length(grep("GO", enrichterm)) > 0){
-        
+
         # GO terms between two categories #
-        formula_go <- compareCluster(Entrez~group + othergroup, data = neweg, fun = "enrichGO", ont = "all", 
+        formula_go <- compareCluster(Entrez~group + othergroup, data = neweg, fun = "enrichGO", ont = "all",
                                      pvalueCutoff = adjustpvaluecut, OrgDb = Dbannotation)
-        clusterProfiler::dotplot(formula_go, x = ~group, color = "p.adjust", showCategory = listnum, split = NULL, font.size = 14, 
+        clusterProfiler::dotplot(formula_go, x = "group", color = "p.adjust", showCategory = listnum, split = NULL, font.size = 14,
                 title = title) + ggplot2::facet_grid(~othergroup)
-        
+
       }else if(length(grep("pathway", enrichterm)) > 0){
-        
+
         # pathways between two categories #
-        formula_path <- compareCluster(Entrez~group + othergroup, data = neweg, fun = "enrichKEGG", 
+        formula_path <- compareCluster(Entrez~group + othergroup, data = neweg, fun = "enrichKEGG",
                                        organism = keggorganism, pvalueCutoff = adjustpvaluecut)
-        clusterProfiler::dotplot(formula_path, x=~group, color = "p.adjust", showCategory = listnum, split = NULL, font.size = 14,
+        clusterProfiler::dotplot(formula_path, x = "group", color = "p.adjust", showCategory = listnum, split = NULL, font.size = 14,
                 title = title) + ggplot2::facet_grid(~othergroup)
       }
     }
@@ -145,4 +144,3 @@ Enrich_plot <- function(regiongenealls_significant, adjustpvaluecut = 0.1, enric
 
 
 
- 
--- R/Quick_start.R
+++ R/Quick_start.R
@@ -1,56 +1,13 @@
-#' Gene-based differentially methylated regions analysis (GeneDMRs) and install the dependencies.
-#' 
-#' @description GeneDMRs is an R package to detect the differentially methylated regions based on genes (DMG), gene body (DMP, DME, DMI), 
-#' CpG islands and gene body interacted with CpG island features (e.g., DMG/DMP/DME/DMI_CpG island and DMG/DMP/DME/DMI_CpG island shore). 
-#' This function can install the other R packages for the dependencies of GeneDMRs.
-#' 
-#' @param Dbannotation refers to the annotation dataset for enrichment, with default "org.Mm.eg.db" of mouse.
-#' 
-#' @return Outputs a list of required R packages.
-#'
-#' @examples
-#' GeneDMRs(Dbannotation = "org.Mm.eg.db")
-#' 
-#' @export
-
-
-GeneDMRs <- function(Dbannotation = "org.Mm.eg.db"){
-  
-  # install the dependencies and library them #
-  if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-  BiocManager::install(c("devtools", "clusterProfiler", "corrplot", "dplyr", "ffbase", "genomation", "KEGG.db", 
-                         "pheatmap", "plotrix", "qqman", "RCircos", "VennDiagram", Dbannotation))
-  
-  require("clusterProfiler")
-  require("corrplot")
-  require("devtools")
-  require("dplyr")
-  require("ffbase")
-  require("genomation")
-  require("KEGG.db")
-  require("pheatmap")
-  require("plotrix")
-  require("qqman")
-  require("RCircos")
-  require("VennDiagram")
-  require(Dbannotation)
-}
-
-
-
-
-
 #' Quick use the GeneDMRs package for gene based differentially methlated regions.
-#' 
+#'
 #' @import dplyr
 #' @import ffbase
 #' @import qqman
 #' @import pheatmap
 #' @import clusterProfiler
-#' 
+#'
 #' @description This function outputs a series of results and figures for gene based regions' methylation analysis.
-#' 
+#'
 #' @param paths refers to the path of input file, with default the package path.
 #' @param control_paths refers to the path of control groups, with default NULL.
 #' @param case_paths refers to the path of case groups, with default NULL.
@@ -59,121 +16,121 @@ GeneDMRs <- function(Dbannotation = "org.Mm.eg.db"){
 #' @param suffixbed refers to the suffix of bed file, e.g., ".gz", ".zip" and so on (some files are in text .txt format, then ".txt" or ".txt.gz"), with default ".txt".
 #' @param Dbannotation refers to the annotation dataset for enrichment, with default "org.Mm.eg.db" of mouse.
 #' @param keggorganism refers to the species name for KEGG enrichment, with default "mmu" of mouse.
-#' 
+#'
 #' @return Outputs a series of DMG results.
 #'
 #' @examples
 #' allDMGs <- Quick_GeneDMRs()
 #' allDMGs_mouse <- Quick_GeneDMRs(Dbannotation = "org.Mm.eg.db", keggorganism = "mmu")
-#' 
+#'
 #' # if only case and control group (n = 2) paths are provided #
 #' controls <- c("C:/Users/GeneDMRs/methdata/1_1.gz", "C:/Users/GeneDMRs/methdata/1_2.gz", "C:/Users/GeneDMRs/methdata/1_3.gz")
 #' cases <- c("C:/Users/GeneDMRs/methdata/2_1.gz", "C:/Users/GeneDMRs/methdata/2_1.gz")
 #' allDMGs <- Quick_GeneDMRs(paths = "C:/Users/GeneDMRs/methdata", control_paths = controls, case_paths = cases)
-#' 
+#'
 #' @export
 
 
 Quick_GeneDMRs <- function(paths = paste(system.file(package = "GeneDMRs"), "/methdata", sep=""), suffixmeth = ".gz",
-                           control_paths = NULL, case_paths = NULL, bedfile = "refseq", 
+                           control_paths = NULL, case_paths = NULL, bedfile = "refseq",
                            suffixbed = ".txt", Dbannotation = "org.Mm.eg.db", keggorganism = "mmu"){
-  
+
   # read the file #
   # if case and control group paths are provided #
   if(is.null(control_paths) == F & is.null(case_paths) == F){
     inputmethfile <- Methfile_read(control_paths = controls, case_paths = cases)
-    
+
   }else{
     inputmethfile <- Methfile_read(paths = paths, suffix = suffixmeth)
   }
-  
+
   inputrefseqfile <- Bedfile_read(paths = paths, bedfile = bedfile, suffix = suffixbed, feature = FALSE)
-  
+
   # quality control #
   inputmethfile_QC <- Methfile_QC(inputmethfile)
-  
+
   # methylation mean #
   regiongeneall <- Methmean_region(inputmethfile_QC, inputrefseqfile, chrnum = "all")
-  
+
   # statistical test #
   regiongeneall_Qvalue <- Logic_regression(regiongeneall)
-  
+
   # sifnificant filter #
   regiongeneall_significant <- Significant_filter(regiongeneall_Qvalue)
-  
+
   # visualization #
   Group_boxplot(regiongeneall)
   Manhattan_plot(regiongeneall_Qvalue, chrlabs = c(1:19,"X","Y"), col = c("red","blue"), genomewideline = -log10(1e-02))
   Chromosome_pieplot(regiongeneall_significant, title = "Distribution of DMG")
   Heatmap_plot(regiongeneall_significant, title = "Methylation level (%) for genes", display_numbers = FALSE)
-  
+
   Enrich_plot(regiongeneall_significant, enrichterm = "GOgroup", Dbannotation = Dbannotation, title = "Biological process for significant gene")
-  Enrich_plot(regiongeneall_significant, enrichterm = "GO", Dbannotation = Dbannotation, title = "Go term for significant gene") 
+  Enrich_plot(regiongeneall_significant, enrichterm = "GO", Dbannotation = Dbannotation, title = "Go term for significant gene")
   Enrich_plot(regiongeneall_significant, enrichterm = "pathway", keggorganism = keggorganism, title = "Pathway for significant gene")
-  
+
   return(regiongeneall_significant)
-  
+
   print(paste("The finish time is", date(), sep = " "))
 }
 
 
 
 
- 
+
 #' Quick use the GeneDMRs package for differentially methylated cytosine sites.
-#' 
+#'
 #' @import dplyr
 #' @import qqman
-#' 
+#'
 #' @description This function outputs the differentially methylated cytosine sites (DMCs).
-#' 
+#'
 #' @param paths refers to the path of input file, with default the package path.
 #' @param control_paths refers to the path of control groups, with default NULL.
 #' @param case_paths refers to the path of case groups, with default NULL.
 #' @param suffixmeth refers to the suffix of methylation file, e.g., ".gz", ".zip" and so on (some files are in text .txt format, then ".txt" or ".txt.gz"), with default ".gz".
-#' 
+#'
 #' @return Outputs DMC results.
 #'
 #' @examples
 #' allDMCs <- Quick_DMCs()
-#' 
+#'
 #' # if only case and control group (n = 2) paths are provided #
 #' controls <- c("C:/Users/GeneDMRs/methdata/1_1.gz", "C:/Users/GeneDMRs/methdata/1_2.gz", "C:/Users/GeneDMRs/methdata/1_3.gz")
 #' cases <- c("C:/Users/GeneDMRs/methdata/2_1.gz", "C:/Users/GeneDMRs/methdata/2_1.gz")
 #' allDMCs <- Quick_DMCs(control_paths = controls, case_paths = cases)
-#' 
+#'
 #' @export
 
 
 Quick_DMCs <- function(paths = paste(system.file(package = "GeneDMRs"), "/methdata", sep=""), suffixmeth = ".gz",
                        control_paths = NULL, case_paths = NULL){
-  
+
   # read the file #
   # if case and control group paths are provided #
   if(is.null(control_paths) == F & is.null(case_paths) == F){
     inputmethfile <- Methfile_read(control_paths = controls, case_paths = cases)
-    
+
   }else{
     inputmethfile <- Methfile_read(paths = paths, suffix = suffixmeth)
   }
-  
+
   # quality control #
   inputmethfile_QC <- Methfile_QC(inputmethfile)
-  
+
   # methylation mean #
   siteall <- Methmean_site(inputmethfile_QC)
-  
+
   # statistical test #
   siteall_Qvalue <- Logic_regression(siteall)
-  
+
   # sifnificant filter #
   siteall_significant <- Significant_filter(siteall_Qvalue, methdiff = 0.1)
-  
+
   # visualization #
   Manhattan_plot(siteall_Qvalue, chrlabs = c(1:19,"X","Y"), col = c("red","blue"), genomewideline = -log10(1e-02))
   Volcano_plot(siteall_Qvalue)
   Chromosome_pieplot(siteall_significant, title = "Distribution of DMC")
-  
+
   return(siteall_significant)
   print(paste("The finish time is", date(), sep = " "))
 }
